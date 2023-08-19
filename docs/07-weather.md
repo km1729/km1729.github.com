e1015b98cf993dae7ad5b02e1e70de073df5c2ff
kmcin <kellymcintyre@outlook.com.au>
2023-08-19 22:24:50 +1000

    docs: update weater.md

--- docs/07-weather.md
+++ docs/07-weather.md
@@ -1,3 +1,9 @@
 # Weather  
+- geolocation of user
+- navigator.geolocation.getCurrentPosition()
+    - this function allows to collect location, wifi ...
+    - search on google getCurrentPosition
+    - https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+- getCurrentPosition()을 이용하면 position object를 가져올 수 있다. 콘솔에서 호가인해보면 
 
 https://openweathermap.org/current
--- js/weather.js
+++ js/weather.js
@@ -1,23 +1,26 @@
+const API_KEY = "c6deb5fbaddb5eabf993577e75121e15"
 const locaton = navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
-
-const API_KEY = ""
 
 function onGeoOk(position){
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  console.log(`You live in ${lat}, ${lon}`);  
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  fetch(url)
-    .then(response => response.json())
-    .then(data =>{
-      const weatherContainer = document.querySelector("#weather span:first-child")
-      const cityContainer = document.querySelector("#weather span:first-child")
-    city.innerText = data.name;
-    weather.innerText = `${data.weather[0].main} / ${data.}`;
-  });
+  console.log(position)
+  // console.log(`You live in ${lat}, ${lon}`);  
+  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  // fetch(url)
+  //   .then(response => response.json())
+  //   .then(data =>{
+  //     const weatherContainer = document.querySelector("#weather span:first-child")
+  //     const cityContainer = document.querySelector("#weather span:first-child")
+  //   city.innerText = data.name;
+  //   weather.innerText = `${data.weather[0].main} / ${data.main}`;
+  // });
 }
 
 function onGeoError(){
-  
+  // alert("Can't find your location")
 }
 
+
+
+console.log("weather");
